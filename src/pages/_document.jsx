c7e48a3a6d5327e8823b2c7540a9491efc7af9cc
Alex Bicknell <60597067+kuuzon@users.noreply.github.com>
2023-05-16 14:26:30 +1000

    Favicon update

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
