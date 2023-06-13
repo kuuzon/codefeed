75cbe394ba048da4d5c5b9234da07ef71e2ab5eb
Alex Bicknell <60597067+kuuzon@users.noreply.github.com>
2023-06-14 00:33:36 +1000

    Init host

--- src/pages/[articleId]/index.jsx
+++ src/pages/[articleId]/index.jsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import ArticleDetail from '@/components/feature/articles/ArticleDetail/ArticleDetail';
 
 function ArticleDetailPage(props) {
-  const { article } = props;
+  // const { article } = props;
 
   return (
     <Fragment>
@@ -14,58 +14,58 @@ function ArticleDetailPage(props) {
           description={article.title + ': ' + article.description}
         />
       </Head>
-      <ArticleDetail 
+      {/* <ArticleDetail 
         image={article.image}
         title={article.title}
         description={article.description}
         category={article.category}
-      />
+      /> */}
     </Fragment>
   );
 }
 
 // STATIC SITE GENERATION (snippet: "ngsp")
-export const getStaticProps = async ( context ) => {
-  // (a) Fetches ENTIRE articles array from INTERNAL API
-  const response = await fetch(`${process.env.SERVER_NAME}/api/articles`);
-  const articles = await response.json();
-  console.log(articles)
+// export const getStaticProps = async ( context ) => {
+//   // (a) Fetches ENTIRE articles array from INTERNAL API
+//   const response = await fetch(`${process.env.SERVER_NAME}/api/articles`);
+//   const articles = await response.json();
+//   console.log(articles)
 
-  // (b) Store params id value (article USER wants!)
-  const articleQuery = context.params.articleId;
+//   // (b) Store params id value (article USER wants!)
+//   const articleQuery = context.params.articleId;
 
-  // (c) Filters articles array to match & return article passed in params
-  const articleMatch = articles.filter(
-    (article) => article._id.toString() === articleQuery 
-  )
+//   // (c) Filters articles array to match & return article passed in params
+//   const articleMatch = articles.filter(
+//     (article) => article._id.toString() === articleQuery 
+//   )
 
-  return {
-    props: {
-      article: articleMatch[0]
-    },
-  };
-};
+//   return {
+//     props: {
+//       article: articleMatch[0]
+//     },
+//   };
+// };
 
 // STATIC SITE DYNAMIC PATHS (snippet: "ngspa")
-export const getStaticPaths = async () => {
-  // (a) Fetches ENTIRE articles array from INTERNAL API
-  const response = await fetch(`${process.env.SERVER_NAME}/api/articles`);
-  const articles = await response.json();
+// export const getStaticPaths = async () => {
+//   // (a) Fetches ENTIRE articles array from INTERNAL API
+//   const response = await fetch(`${process.env.SERVER_NAME}/api/articles`);
+//   const articles = await response.json();
 
-  // (b) Pull ALL the ids out of the articles array ONLY
-  const idList = articles.map((article) => article._id);
-  console.log(idList);
+//   // (b) Pull ALL the ids out of the articles array ONLY
+//   const idList = articles.map((article) => article._id);
+//   console.log(idList);
 
-  // (c) Pre-build ALL the URL paths for all existing ids in array 
-  const paths = idList.map((id) => (
-    { params: { articleId: id.toString() }}
-  ));
-  // NOTE: The id MUST be converted to a string, as URLs need strings NOT numbers!
+//   // (c) Pre-build ALL the URL paths for all existing ids in array 
+//   const paths = idList.map((id) => (
+//     { params: { articleId: id.toString() }}
+//   ));
+//   // NOTE: The id MUST be converted to a string, as URLs need strings NOT numbers!
 
-  return {
-    paths,
-    fallback: false
-  }
-}
+//   return {
+//     paths,
+//     fallback: false
+//   }
+// }
 
 export default ArticleDetailPage;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,7 @@ import HeroSection from '@/components/layout/HeroSection/HeroSection';
 import ArticlesList from '@/components/feature/articles/ArticlesList/ArticleList';
 
 function HomePage(props) {
-  const { articles } = props 
+  // const { articles } = props 
 
   return (
     <Fragment>
@@ -20,24 +20,24 @@ function HomePage(props) {
         description='Catch up on all the coding news from around the globe, at the touch of a button'
         bgImage="/backgrounds/hero-code.webp"
       />
-      {articles.length > 0 && <ArticlesList articles={articles} />}
+      {/* {articles.length > 0 && <ArticlesList articles={articles} />} */}
     </Fragment>
   )
 }
 
 // STATIC SITE GENERATION (snippet: "ngsp")
-export const getStaticProps = async () => {
-  // Fetch data from Internal API ("Code News")
-  const response = await fetch(`${process.env.SERVER_NAME}/api/articles`);
-  const data = await response.json();
+// export const getStaticProps = async () => {
+//   // Fetch data from Internal API ("Code News")
+//   const response = await fetch(`${process.env.SERVER_NAME}/api/articles`);
+//   const data = await response.json();
 
-  // Returned data as props & ISR functionality
-  return {
-    props: {
-      articles: data
-    },
-    revalidate: 60 * 60,
-  };
-};
+//   // Returned data as props & ISR functionality
+//   return {
+//     props: {
+//       articles: data
+//     },
+//     revalidate: 60 * 60,
+//   };
+// };
 
 export default HomePage;
