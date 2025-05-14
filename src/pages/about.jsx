5f3b13a9e4cea0416fbfb809fbf0ff226b8b287b
Alex Bicknell <60597067+kuuzon@users.noreply.github.com>
2025-05-14 21:28:22 +1000

    initial about contact page builds

---
+++ src/pages/about.jsx
@@ -0,0 +1,12 @@
+// [2A] BASIC ROUTES - URL: https://localhost:3000/about
+import Container from "@/components/common/Container";
+
+const AboutPage = () => {
+  return (
+    <Container>
+      <h1>About Us</h1>
+      <p>Under construction!</p>
+    </Container>
+  );
+};
+export default AboutPage;
---
+++ src/pages/contact.jsx
@@ -0,0 +1,12 @@
+// [2A] BASIC ROUTES - URL: https://localhost:3000/about
+import Container from "@/components/common/Container";
+
+const ContactPage = () => {
+  return (
+    <Container>
+      <h1>Contact Us</h1>
+      <p>Under contruction!</p>
+    </Container>
+  )
+};
+export default ContactPage;
